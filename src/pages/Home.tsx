b2003cce6da11f179c52765a26b34f2f72cfb008
yoonseo <yoonseo1004s@gmail.com>
2024-09-07 16:19:36 +0900

    todocheck

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import BookmarkedList from "../components/BookmarkedList";
 import Todo from "../components/home/Todo/Todo";
 
 const Home = () => {
   return (
     <>
-      <h1>Home Component </h1>
+      <h1>Home Component</h1>
+      <BookmarkedList />
       <Todo />
     </>
   );
