d23cc2db3bf2fb0c12efe555cf18af5a69bebd40
yoonseo <yoonseo1004s@gmail.com>
2024-09-08 00:53:32 +0900

    hard try checkbox...

--- src/components/home/Todo/Checkbox.tsx
+++ src/components/home/Todo/Checkbox.tsx
@@ -13,7 +13,7 @@ const TodoCheckbox = ({ className, children, ...rest }: CheckboxType) => {
       <input
         id={uid}
         className={twMerge(
-          "appearance-none w-4 h-4  rounded bg-[url('/check.svg')] bg-no-repeat bg-center checked:bg-[url('/check_black.svg')] shrink-0",
+          "appearance-none w-4 h-4 border border-[#636363] rounded-[4px] bg-no-repeat bg-center checked:border-4 checked:bg-[url('/check_black.svg')] shrink-0",
           className
         )}
         {...rest}
