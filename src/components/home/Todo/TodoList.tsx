0339fe28161e8d7efc7da3366fbfb7bf5ebfc4f6
yoonseo <yoonseo1004s@gmail.com>
2024-09-08 00:48:15 +0900

    check box test02

--- src/components/home/Todo/TodoList.tsx
+++ src/components/home/Todo/TodoList.tsx
@@ -21,17 +21,8 @@ const TodoListPart: React.FC<TodoListPartProps> = ({
             className="flex items-center justify-between p-2 bg-white rounded shadow"
           >
             <TodoCheckbox type="checkbox" />
-            <span
-              className={`${
-                todo.completed ? "line-through text-gray-500" : ""
-              }`}
-            >
-              {todo.text}
-            </span>
-            <button
-              onClick={() => onToggleTodo(todo.id)}
-              className="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-            ></button>
+            <span>{todo.text}</span>
+            <button onClick={() => onToggleTodo(todo.id)}></button>
           </li>
         ))}
       </ul>
