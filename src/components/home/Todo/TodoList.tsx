3d145f6b3fe8ced225a6ba9f558462aa7805a88f
yoonseo <yoonseo1004s@gmail.com>
2024-09-09 19:37:00 +0900

    color 수정

--- src/components/home/Todo/TodoList.tsx
+++ src/components/home/Todo/TodoList.tsx
@@ -1,5 +1,5 @@
 import { TodoItem } from "../types/types";
-import TodoCheckbox, { testFunction, testFunction2 } from "./Checkbox";
+import TodoCheckbox from "./Checkbox";
 
 interface TodoListPartProps {
   todos: TodoItem[];
@@ -8,24 +8,28 @@ interface TodoListPartProps {
 }
 
 const TodoListPart: React.FC<TodoListPartProps> = ({
-  todos,
+  todos = [],
   onToggleTodo,
   className,
 }) => {
   return (
     <div className={`${className} flex flex-col`}>
-      <ul className="space-y-2">
-        {todos.map((todo) => (
-          <li
-            key={todo.id}
-            className="flex items-center justify-between p-2 bg-white rounded shadow"
-          >
-            <TodoCheckbox type="checkbox" />
-            <span>{todo.text}</span>
-            <button onClick={() => onToggleTodo(todo.id)}></button>
-          </li>
-        ))}
-      </ul>
+      {todos.length === 0 ? (
+        <p className="text-center text-gray-500">오늘 할 일 목록이 없습니다</p>
+      ) : (
+        <ul className="space-y-2">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex items-center p-2 bg-white rounded shadow"
+            >
+              <TodoCheckbox type="checkbox" />
+              <span className="text-gray-500 ml-2">{todo.text}</span>
+              <button onClick={() => onToggleTodo(todo.id)}></button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
