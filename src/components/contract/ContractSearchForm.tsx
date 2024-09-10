1e8c8468db4387a95c53c36b457e209c40427536
Attainy <111291076+Attainy@users.noreply.github.com>
2024-09-11 02:34:51 +0900

    [fix] 병합 에러 수정중

---
+++ src/components/contract/ContractSearchForm.tsx
@@ -0,0 +1,17 @@
+const ContractSearchForm = () => {
+  return (
+    <>
+      <h2>계약서 검색</h2>
+      <div>
+        <div>날짜조회</div>
+        <select name="" id=""></select>
+        <div>
+          <input type="date" />
+        </div>
+        <span>~</span>
+        <input type="date" />
+      </div>
+    </>
+  );
+};
+export default ContractSearchForm;
--- src/pages/Contract.tsx
+++ src/pages/Contract.tsx
@@ -1,7 +1,10 @@
+import ContractSearchForm from "../components/contract/ContractSearchForm";
+
 const Contract = () => {
   return (
     <>
       <h1>Contract Component</h1>
+      <ContractSearchForm />
     </>
   );
 };
