d31e79325d02611f2139f298ebb139db4d674494
titch <sujinlee0202@gmail.com>
2024-09-13 22:53:23 +0900

    [fix] api fetch 수정

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,21 +1,22 @@
 import { SearchRegisterInputs } from "../types/register";
 
-const BASE_URL = "http://35.193.35.53";
-
 export const searchRegister = async (search: SearchRegisterInputs) => {
   search.address = "궁동 401-2";
 
   try {
-    const response = await fetch(`${BASE_URL}/juso/search`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        juso: search.address,
-        page_no: 5,
-      }),
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_BASE_URL}/juso/search`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          juso: search.address,
+          page_no: 5,
+        }),
+      }
+    );
 
     const data = await response.json();
 
@@ -27,7 +28,7 @@ export const searchRegister = async (search: SearchRegisterInputs) => {
 
 export const checkOpenedRegister = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/pdf-list`, {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/pdf-list`, {
       method: "GET",
     });
 
