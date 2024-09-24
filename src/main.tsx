6bbb3b8d8d552f66991bbf440edf0851ca5308ac
yoonseo <yoonseo1004s@gmail.com>
2024-09-24 21:21:27 +0900

    reedit

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const queryClient = new QueryClient();
-const CLIENT_ID =
-  "851060147388-8tl0fa5scviqmehsn5hodac94n837q35.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.CLIENT_ID}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
