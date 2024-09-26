715f415add8d571839520f51380be326d13cbc84
racheljeong <jdsss2634@gmail.com>
2024-09-26 11:37:52 +0900

    [feat] url수정, 외부링크추가

--- src/components/bookmark/BookmarkedEachItem.tsx
+++ src/components/bookmark/BookmarkedEachItem.tsx
@@ -3,16 +3,16 @@ import { CSS } from '@dnd-kit/utilities';
 import { bookmarkDataInterface } from '../../types/bookmark';
 import { HiOutlineBookmarkAlt } from "react-icons/hi";
 
-const BookmarkedEachItem = ({bookmarkId, bookmarkOgImg, bookmarkTitle}: bookmarkDataInterface) => {
+const BookmarkedEachItem = ({bookmarkId, bookmarkURL,bookmarkTitle,bookmarkOgImg}: bookmarkDataInterface) => {
   
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({id: bookmarkId});
  
   const ogImg = (bookmarkOgImg !== "이미지 없음") ? bookmarkOgImg : null;
-
+  console.log(bookmarkURL);
   return (
     <>    
       <div className="w-[180px] h-[180px] relative bg-cover bg-center border-slate-400 border-[1px] rounded-2xl"
-           style={{ 
+          style={{ 
              backgroundImage: ogImg ? `url(${ogImg})` : 'none',
              backgroundSize: 'contain',
              backgroundRepeat: 'no-repeat',
@@ -20,13 +20,14 @@ const BookmarkedEachItem = ({bookmarkId, bookmarkOgImg, bookmarkTitle}: bookmark
              transition: transition ? transition : undefined
            }}
            ref={setNodeRef} {...attributes} {...listeners}>
-        
-        {!ogImg && (
-          <div className="flex justify-center items-center w-full h-full">
-            <HiOutlineBookmarkAlt className="w-[80px] h-[80px]" 
-                                  style={{ color: '#cbd5e1', strokeWidth: '1' }}/>
-          </div>
-        )}
+        <a href={bookmarkURL} target="_blank" rel="noopener noreferrer" className="block w-full h-full">
+          {!ogImg && (
+            <div className="flex justify-center items-center w-full h-full">
+              <HiOutlineBookmarkAlt className="w-[80px] h-[80px]" 
+                                    style={{ color: '#cbd5e1', strokeWidth: '1' }}/>
+            </div>
+          )}
+        </a>
       </div>
       <span className="absolute w-[181px] h-[34px] text-center py-3">{bookmarkTitle}</span>
     </>
